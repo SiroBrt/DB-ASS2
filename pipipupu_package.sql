2c260a47432c5f711595131b9dde1f1987333d70
Yan Chen Zhou <2205155148yan@gmail.com>
2025-03-19 19:50:30 +0100

    random package code templete that does not work

--- pipipupu_package.sql
+++ pipipupu_package.sql
@@ -2,31 +2,59 @@
 -- ------------------------------------------------------------------------------------
 
 CREATE OR REPLACE PACKAGE BODY foundicu IS 
+    -- TYPE my_type IS RECORD (...);
+    current_user CHAR(10);
+    PROCEDURE insert_loan(signature IN loans.signature%TYPE);
+    PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in date);
+    PROCEDURE record_books_returning(signature IN loans.signature%TYPE);
+END foundicu;
 
-    PROCEDURE insert_loan(signature IN VARCHAR2)
-        IS PRAGMA AUTONOMOUS_TRANSACTION;
-        -- BEGIN
 
+
+
+CREATE OR REPLACE PACKAGE BODY foundicu IS
+
+    PROCEDURE insert_loan(signature IN loans.signature%TYPE) IS
+        user users%ROWTYPE;
+    BEGIN
+        -- SELECT * INTO user FROM users u
+        --     WHERE u.user_id = current_user;
+        
+        -- SELECT signature, user_id, stopdate, type FROM loans l
+        -- WHERE l.signature = signature AND l.user_id = current_user;
+        -- INSERT INTO LOANS VALUES (signature, user_id, SYSDATE, town, province, type, time, return);
         -- COMMIT;
+    -- EXCEPTION
     END insert_loan;
 
-    PROCEDURE insert_reservation(isbn IN varchar2, reservation_date in date)
-        IS PRAGMA AUTONOMOUS_TRANSACTION;
-        -- BEGIN
+    PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in DATE) IS
+    BEGIN
+        SELECT * INTO user FROM users u
+            WHERE u.user_id = current_user;
 
         -- COMMIT;
     END insert_reservation;
 
-    PROCEDURE record_books_returning(signature IN VARCHAR2)
-        IS PRAGMA AUTONOMOUS_TRANSACTION;
-        -- BEGIN
-
+    PROCEDURE record_books_returning(signature IN loans.signature%TYPE) IS
+        signature loans.signature%TYPE;
+    BEGIN
+        SELECT signature, user_id, stopdate FROM loans l
+            WHERE l.signature = signature AND l.user_id = current_user AND l.type = 'L';
+        UPDATE loans SET return = SYSDATE 
+            WHERE signature = signature AND user_id = current_user;
         -- COMMIT;
+
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            dbms_output.put_line('No data found');
+        WHEN OTHERS THEN
+            dbms_output.put_line('An error has occurred');
     END record_books_returning;
 
-BEGIN	
-    dbms_output.put_line('Control is now executing the package initialization part');
-END foundicu;
+END package_name;
+-- BEGIN	
+--     dbms_output.put_line('Control is now executing the package initialization part');
+-- END foundicu;
 
 -- Printing 
 -- SET SERVEROUTPUT ON;

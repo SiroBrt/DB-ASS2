0c4a1b656d4ad5d150e36442bd7391ccc4afa4b0
Yan <62068603+Fnixya@users.noreply.github.com>
2025-03-26 21:54:47 +0100

    yeah fuck it

--- pipipupu_package.sql
+++ pipipupu_package.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE PACKAGE foundicu AS
     current_user CHAR(10);
     PROCEDURE insert_loan(signature IN loans.signature%TYPE);
     -- PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in date);
-    -- PROCEDURE record_books_returning(signature IN loans.signature%TYPE);
+    PROCEDURE record_books_returning(signature IN loans.signature%TYPE);
 
     -- PROCEDURE set_current_user(new_user IN current_user%TYPE);
     -- FUNCTION get_current_user RETURN CHAR;
@@ -15,6 +15,8 @@ END foundicu;
 
 -- PACKAGE BODY
 CREATE OR REPLACE PACKAGE BODY foundicu AS
+
+    -- THIS SHIT IS NOT FINISHED AND I HATE THIS PUTA MIERDA
     PROCEDURE insert_loan(signature IN loans.signature%TYPE) IS
         user_count NUMBER;
         reservated NUMBER;
@@ -24,12 +26,14 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
         user_is_banned EXCEPTION;
         copy_not_available EXCEPTION;
     BEGIN
+        -- CHECK IF USER EXISTS (THROUGH USER_COUNT VALUE) AND IF THE USER IS BANNED
         SELECT COUNT(*), BAN_UP2 INTO user_count, ban_date FROM users WHERE users.user_id = current_user;
         
         IF user_count = 0 
             THEN RAISE user_does_not_exist;
         END IF;
         
+        -- CHECK IS THERE IS AN EXISTING RESERVATION FOR THE BOOK
         SELECT COUNT(*) INTO reservated FROM loans l
             WHERE l.signature = signature 
                 AND l.user_id = current_user
@@ -39,7 +43,7 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
             THEN UPDATE LOANS l SET TYPE='L' WHERE l.user_id=current_user AND l.signature=signature;
         ELSIF SYSDATE < ban_date
             THEN RAISE user_is_banned;
-        ELSE
+        -- ELSE
             -- to insert new loan, we need to insert a new route and assign drv
             -- INSERT INTO LOANS VALUES(signature, user_id, SYSDATE, town, province, type, time, return);
         END IF;
@@ -54,7 +58,6 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
         WHEN user_is_banned 
             THEN dbms_output.put_line('Current user is banned'); 
     END insert_loan;
-END foundicu;
 
     -- PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in DATE) IS
     -- BEGIN
@@ -64,20 +67,35 @@ END foundicu;
     --     -- COMMIT;
     -- END insert_reservation;
 
-    -- PROCEDURE record_books_returning(signature IN loans.signature%TYPE) IS
-    --     signature loans.signature%TYPE;
-    -- BEGIN
-    --     SELECT signature, user_id, stopdate FROM loans l
-    --         WHERE l.signature = signature AND l.user_id = current_user AND l.type = 'L';
-    --     UPDATE loans SET return = SYSDATE 
-    --         WHERE signature = signature AND user_id = current_user;
-    --     -- COMMIT;
-    -- EXCEPTION
-    --     WHEN NO_DATA_FOUND THEN
-    --         dbms_output.put_line('No data found');
-    --     WHEN OTHERS THEN
-    --         dbms_output.put_line('An error has occurred');
-    -- END record_books_returning;
+    -- IT SHOULD BE OK UNLESS WE THINK OF OTHER IMPEDIMENTS
+    PROCEDURE record_books_returning(signature IN loans.signature%TYPE) IS
+        existing_loan NUMBER;
+        return_value loans.return%TYPE;
+        no_loan EXCEPTION;
+        book_already_returned EXCEPTION;
+    BEGIN
+        -- CHECK IF THE BOOK IS BEING LOANED BY CURRENT USER
+        SELECT COUNT(*), return INTO existing_loan, return_value FROM loans l
+            WHERE l.signature = signature 
+                AND l.user_id = current_user 
+                AND l.type = 'L';
+
+        IF existing_loan = 0
+            THEN RAISE no_loan;
+        ELSIF return_value IS NOT NULL
+            THEN RAISE book_already_returned;
+        END IF;
+
+        -- UPDATE RETURN OF LOAN
+        UPDATE loans SET return = SYSDATE 
+            WHERE signature = signature AND user_id = current_user;
+        COMMIT;
+    EXCEPTION
+        WHEN no_loan THEN
+            dbms_output.put_line('Error. Current user has not borrowed this copy of the book.');
+        WHEN book_already_returned THEN
+            dbms_output.put_line('Aborted. The book has already been returned.');
+    END record_books_returning;
 
     
     -- PROCEDURE set_current_user(new_user IN CHAR(10)) IS
@@ -86,8 +104,7 @@ END foundicu;
     -- END set_current_user;
 
     -- FUNCTION get_current_user RETURN CHAR;
-
--- END foundicu;
+END foundicu;
 -- BEGIN	
 --     dbms_output.put_line('Control is now executing the package initialization part');
 -- END foundicu;

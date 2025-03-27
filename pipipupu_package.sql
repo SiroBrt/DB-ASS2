53037c7ba98faee0005284f9add31b3fd84a4822
Yan <62068603+Fnixya@users.noreply.github.com>
2025-03-27 16:35:52 +0100

    1.2.3 exception cases work good

--- pipipupu_package.sql
+++ pipipupu_package.sql
@@ -5,19 +5,19 @@
 CREATE OR REPLACE PACKAGE foundicu AS 
     -- TYPE my_type IS RECORD (...);
     current_user CHAR(10);
-    PROCEDURE insert_loan(signature IN loans.signature%TYPE);
+    PROCEDURE insert_loan(copy_signature IN loans.signature%TYPE);
     PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in date);
-    PROCEDURE record_books_returning(signature IN loans.signature%TYPE);
+    PROCEDURE record_books_returning(copy_signature IN loans.signature%TYPE);
 
-    -- PROCEDURE set_current_user(new_user IN current_user%TYPE);
-    -- FUNCTION get_current_user RETURN CHAR;
+    PROCEDURE set_current_user(new_user IN current_user%TYPE);
+    FUNCTION get_current_user RETURN current_user%TYPE;
 END foundicu;
 
 -- PACKAGE BODY
 CREATE OR REPLACE PACKAGE BODY foundicu AS
 
     -- THIS SHIT IS NOT FINISHED AND I HATE THIS PUTA MIERDA
-    PROCEDURE insert_loan(signature IN loans.signature%TYPE) IS
+    PROCEDURE insert_loan(copy_signature IN loans.signature%TYPE) IS
         user_count NUMBER;
         reservated NUMBER;
         ban_date users.BAN_UP2%TYPE;
@@ -27,7 +27,9 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
         copy_not_available EXCEPTION;
     BEGIN
         -- CHECK IF USER EXISTS (THROUGH USER_COUNT VALUE) AND IF THE USER IS BANNED
-        SELECT COUNT(*), BAN_UP2 INTO user_count, ban_date FROM users WHERE users.user_id = current_user;
+        SELECT COUNT(*), BAN_UP2 INTO user_count, ban_date FROM users 
+            WHERE users.user_id = current_user
+            GROUP BY BAN_UP2;
         
         IF user_count = 0 
             THEN RAISE user_does_not_exist;
@@ -35,7 +37,7 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
         
         -- CHECK IS THERE IS AN EXISTING RESERVATION FOR THE BOOK
         SELECT COUNT(*) INTO reservated FROM loans l
-            WHERE l.signature = signature 
+            WHERE l.signature = copy_signature 
                 AND l.user_id = current_user
                 AND l.type = 'R';
 
@@ -50,13 +52,12 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
         
         -- SELECT signature, user_id, stopdate, type FROM loans l
         -- WHERE l.signature = signature AND l.user_id = current_user;
-        
         COMMIT;
     EXCEPTION
-        WHEN user_does_not_exist 
-            THEN dbms_output.put_line('Current user does not exist'); 
+        WHEN NO_DATA_FOUND
+            THEN dbms_output.put_line('Error. Current user does not exist'); 
         WHEN user_is_banned 
-            THEN dbms_output.put_line('Current user is banned'); 
+            THEN dbms_output.put_line('Abort. Current user is banned'); 
     END insert_loan;
 
     PROCEDURE insert_reservation(isbn IN editions.isbn%TYPE, reservation_date in DATE) IS
@@ -76,40 +77,44 @@ CREATE OR REPLACE PACKAGE BODY foundicu AS
     END insert_reservation;
 
     -- IT SHOULD BE OK UNLESS WE THINK OF OTHER IMPEDIMENTS
-    PROCEDURE record_books_returning(signature IN loans.signature%TYPE) IS
-        existing_loan NUMBER;
-        return_value loans.return%TYPE;
-        no_loan EXCEPTION;
-        book_already_returned EXCEPTION;
-    BEGIN
-        -- CHECK IF THE BOOK IS BEING LOANED BY CURRENT USER
-        SELECT COUNT(*), return INTO existing_loan, return_value FROM loans l
-            WHERE l.signature = signature 
-                AND l.user_id = current_user 
-                AND l.type = 'L';
-        IF existing_loan = 0
-            THEN RAISE no_loan;
-        ELSIF return_value IS NOT NULL
-            THEN RAISE book_already_returned;
+    PROCEDURE record_books_returning(copy_signature IN loans.signature%TYPE) IS
+        loan_count NUMBER;
+        no_loan_found EXCEPTION;
+        multiple_loans_found EXCEPTION;
+    BEGIN   
+        -- CHECK IF THE BOOK IS BEING LOANED BY CURRENT USER#
+        SELECT COUNT(1) INTO loan_count FROM loans l
+            WHERE l.signature = copy_signature 
+                AND l.user_id = current_user
+                AND l.return IS NULL;
+        
+        IF loan_count = 0
+            THEN RAISE no_loan_found;
+        ELSIF loan_count > 1
+            THEN RAISE multiple_loans_found;
         END IF;
+
         -- UPDATE RETURN OF LOAN
         UPDATE loans SET return = SYSDATE 
             WHERE signature = signature AND user_id = current_user;
         COMMIT;
     EXCEPTION
-        WHEN no_loan THEN
-            dbms_output.put_line('Error. Current user has not borrowed this copy of the book.');
-        WHEN book_already_returned THEN
-            dbms_output.put_line('Aborted. The book has already been returned.');
+        WHEN no_loan_found THEN
+            dbms_output.put_line('Error. No unreturned loan of this copy by current user has been found.');
+        WHEN multiple_loans_found THEN
+            dbms_output.put_line('Error. Found multiple unreturned loans of the same book by the same user.');
     END record_books_returning;
 
     
-    -- PROCEDURE set_current_user(new_user IN CHAR(10)) IS
-    -- BEGIN
-    --     current_user = new_user;
-    -- END set_current_user;
+    PROCEDURE set_current_user(new_user IN current_user%TYPE) IS
+    BEGIN
+        current_user := new_user;
+    END set_current_user;
 
-    -- FUNCTION get_current_user RETURN CHAR;
+    FUNCTION get_current_user RETURN current_user%TYPE IS 
+    BEGIN
+        RETURN current_user;
+    END get_current_user;
 END foundicu;
 -- BEGIN	
 --     dbms_output.put_line('Control is now executing the package initialization part');

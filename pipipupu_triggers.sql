59d134e754520d25656694a3e4ee6c98c5876e54
Yan <62068603+Fnixya@users.noreply.github.com>
2025-03-28 22:37:18 +0100

    1.4.d trigger template

--- pipipupu_triggers.sql
+++ pipipupu_triggers.sql
@@ -1,2 +1,15 @@
 -- ---------------------- TRIGGERS ---------------------
 -- ----------------------------------------------------
+
+-- 1.4.d
+CREATE OR REPLACE TRIGGER update_book_read
+AFTER INSERT OR UPDATE ON TYPE
+FOR EACH ROW WHEN (NEW.TYPE='L')
+BEGIN
+    -- obtain loan's book title and author
+
+    -- increase reads count 
+    UPDATE books 
+        SET reads=reads+1 
+        WHERE title=title AND author=author
+END;

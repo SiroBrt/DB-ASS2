99aa85990c4eb101e545b20649fe19616bd77731
Yan <62068603+Fnixya@users.noreply.github.com>
2025-03-30 15:53:50 +0200

    1.4.d shold be done

--- pipipupu_triggers.sql
+++ pipipupu_triggers.sql
@@ -7,8 +7,9 @@
 
 
 ---- TASK D
-ALTER TABLE books ADD reads INTEGER;
+ALTER TABLE books ADD reads INTEGER DEFAULT 0;
 
+-- option 1 works
 MERGE INTO books b
 USING (
     SELECT b.title, b.author, COUNT(*) AS new_reads
@@ -22,7 +23,21 @@ ON (b.title = src.title AND b.author = src.author)
 WHEN MATCHED THEN
     UPDATE SET b.reads = src.new_reads;
 
+-- option 2 untested
+UPDATE books b
+SET reads = (
+    SELECT COUNT(*)
+        FROM editions e -- ON b.title = e.title AND b.author = e.author
+        JOIN copies c ON e.isbn = c.isbn
+        JOIN loans l ON l.signature = c.signature
+        WHERE e.title = b.title
+        AND e.author = b.author
+        GROUP BY e.title, e.author
+);
 
+UPDATE books SET reads=0 WHERE reads IS NULL;
+
+-- TESTS
 SELECT COUNT(*) FROM (
     SELECT e.title, e.author, COUNT(*) AS new_reads
         FROM editions e -- ON b.title = e.title AND b.author = e.author
@@ -31,23 +46,6 @@ SELECT COUNT(*) FROM (
         GROUP BY e.title, e.author
 );
 
--- UPDATE books b
--- SET b.reads = (
---     SELECT COUNT(*)
---     FROM editions e
---     JOIN copies c ON e.isbn = c.isbn
---     JOIN loans l ON l.signature = c.signature
---     WHERE e.title = b.title AND e.author = b.author
--- );
-
--- UPDATE (
---     SELECT b.reads, COUNT(*) OVER (PARTITION BY b.title, b.author) AS new_reads
---     FROM books b
---     JOIN editions e ON b.title = e.title AND b.author = e.author
---     JOIN copies c ON e.isbn = c.isbn
---     JOIN loans l ON l.signature = c.signature
--- ) 
--- SET reads = new_reads;
 
 
 -- SELECT e.title, e.author, COUNT(*)
@@ -56,15 +54,22 @@ SELECT COUNT(*) FROM (
 -- JOIN loans l ON l.signature = c.signature
 -- GROUP BY e.title, e.author
 -- HAVING COUNT(*) > 5;
--- 1.4.d
+
 CREATE OR REPLACE TRIGGER update_book_read
-AFTER INSERT OR UPDATE ON TYPE
+AFTER INSERT OR UPDATE OF type ON loans
 FOR EACH ROW WHEN (NEW.TYPE='L')
+DECLARE 
+    loan_title editions.title%TYPE;
+    loan_author editions.author%TYPE;
 BEGIN
     -- obtain loan's book title and author
+    SELECT e.title, e.author INTO loan_title, loan_author FROM editions e
+        JOIN copies c
+        ON e.isbn=c.isbn
+        WHERE c.signature=NEW.signature
 
     -- increase reads count 
     UPDATE books 
         SET reads=reads+1 
-        WHERE title=loan_title AND author=loan_author
+        WHERE title=loan_edition.title AND author=loan_edition.author
 END;
